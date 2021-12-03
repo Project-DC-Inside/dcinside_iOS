6c6db33e655225aaa21f4450e1544816278815e9
wolframhwang <hzw94@naver.com>
2021-12-03 19:44:42 +0900

    Alamofire API 설계 진행 중

---
+++ APIs/APIService.swift
@@ -0,0 +1,35 @@
+//
+//  APIService.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2021/12/03.
+//
+
+import Foundation
+import Alamofire
+
+struct APIService {
+    static let shared = APIService()
+    
+    func loginAPI() {
+        
+    }
+    
+    func singUpAPI() {
+        
+    }
+    
+    func openPage() {
+        
+    }
+    
+    func openCategory() {
+        
+    }
+    
+    
+}
+
+
+// enum 으로 상태도 표현해주자
+
--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet weak var idField: UITextField!
@@ -15,6 +16,7 @@ class ViewController: UIViewController {
     
     @IBAction func loginButton(_ sender: Any) {
         print("LOG IN!")
+        
     }
     @IBAction func signUpButton(_ sender: Any) {
         guard let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "SignUpViewController") else {return}
