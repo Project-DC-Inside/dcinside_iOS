f71b072f74f274c3c2dd204beedbf07789b726a4
wolframhwang <hzw94@naver.com>
2021-12-16 23:08:01 +0900

    Tab Bar Naivgation View Controller 를 추가했읍니다.

---
+++ Controller/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2021/12/16.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
