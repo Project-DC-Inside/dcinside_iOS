7e0c0bad63b2280b3883b0c0f46d70b0a1c89323
wolframhwang <hzw94@naver.com>
2021-12-25 15:20:13 +0900

    feat: Add on RefreshToken Function
    - Token을 Refresh 할수 있는 함수를 APIService에 추가하였습니다.

--- APIs/APIService.swift
+++ APIs/APIService.swift
@@ -15,14 +15,13 @@ enum NetworkResult<T> {
     case networkErr
 }
 
-
 class APIService {
     //최초 생성시, 전역으로 사용가능하게한다.
     static let shared = APIService()
     
     private let HTTPHeaders = ["Content-Type": "application/json"]
     private let baseURL = "http://3.36.205.23:8080"
-    
+ 
     func SingInAPI(singin: Login, compleition: @escaping (NetworkResult<Any>) -> Void) {
         AF.request(baseURL + "/api/v1/auth/signin", method: .post, parameters: singin, encoder: JSONParameterEncoder.default).responseJSON { response in
             switch response.result {
@@ -70,8 +69,22 @@ class APIService {
         }
     }
     
-    func refreshAPI(token: TokenInfo,completion: @escaping (NetworkResult<Any>)-> Void) {
-        let reissueToken = refreshToken(accessToken: token.accessToken, refreshToken: token.refreshToken)
+    func DecodeToken()-> TokenInfo? {
+        let decoder = PropertyListDecoder()
+        guard let encodedToken = UserDefaults.standard.object(forKey: "token") as? Data else { return nil }
+        do {
+            let token = try? decoder.decode(TokenInfo.self, from: encodedToken)
+            return token
+        }catch {
+            return nil
+        }
+    }
+    
+    func RefreshAPI(completion: @escaping (NetworkResult<Any>)-> Void) {
+        print("REFRESH!!")
+        guard let token = DecodeToken() else { return }
+        print("TK",token)
+        let reissueToken = refreshToken(accessToken: token.accessToken, refreshToken: token.refreshToken)        
         AF.request(baseURL + "/api/v1/auth/reissue", method: .post, parameters: reissueToken, encoder: JSONParameterEncoder.default).responseJSON { response in
             switch response.result {
             case .success:
@@ -82,10 +95,10 @@ class APIService {
                     guard let isSuccess = json.success else { return }
                     if isSuccess {
                         guard let result = json.result else { return }
-                        completion(.success(result))
+                        completion(.success(json.result))
                     }else {
                         guard let error = json.error else { return }
-                        completion(.failure(error))
+                        completion(.failure(json.error))
                     }
                 }catch{
                     print("Decode Error Occured")
