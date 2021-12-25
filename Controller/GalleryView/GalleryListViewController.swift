8b337bd89403952565cea4e48ccdaf2ce7249d27
wolframhwang <hzw94@naver.com>
2021-12-26 00:41:24 +0900

    feat: Gallery List를 추가
    - Gallery List Scene을 추가한 Commit

---
+++ Controller/GalleryView/GalleryListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryListViewController.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2021/12/26.
+//
+
+import UIKit
+
+class GalleryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
