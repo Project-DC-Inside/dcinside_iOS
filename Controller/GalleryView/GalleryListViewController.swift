a04054dcdf01bd83d9faf0aeced8bc5eccec195c
wolframhwang <hzw94@naver.com>
2021-12-26 00:46:37 +0900

    feat: GalleryList Scene에 TableView Outlet 추가
    - GalleryList Scene에 Tableview Delegate, DataSource를 추가한 commit

--- Controller/GalleryView/GalleryListViewController.swift
+++ Controller/GalleryView/GalleryListViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class GalleryListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+}
+
+extension GalleryListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //dummy data
+        return 10
+    }
+            
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension GalleryListViewController: UITableViewDelegate {
+    
 }
