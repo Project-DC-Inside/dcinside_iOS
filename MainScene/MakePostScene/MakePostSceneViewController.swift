802ca4f41ca2d72ce08651280fab0aa7ec62aa38
wolframhwang <hzw94@naver.com>
2022-03-13 01:20:00 +0900

    [feat] Make Post Scene Base Code 작성
    Make Post Scene 구성에 필요한 View / View Model 을 작성한 패치입니다.

---
+++ MainScene/MakePostScene/MakePostSceneViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MakePostSceneViewController.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/03/13.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MakePostSceneViewController: UIViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("MakePostScene Error Occured")
+    }
+    
+    private func attribute() {
+        
+    }
+    
+    private func layout() {
+        
+    }
+    
+    func bind() {
+        
+    }
+    
+}
---
+++ MainScene/MakePostScene/MakePostSceneViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  MakePostSceneViewModel.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/03/13.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+
+struct MakePostSceneViewModel {
+    
+}
