7b59680ab79609cad874ae9cb357df807436bf26
wolframhwang <hzw94@naver.com>
2022-03-13 01:37:28 +0900

    [feat] MakePostScene View
    MakePostScene을 TableView를 통해 꾸민 패치입니다.

--- MainScene/MakePostScene/MakePostSceneViewController.swift
+++ MainScene/MakePostScene/MakePostSceneViewController.swift
@@ -11,6 +11,11 @@ import RxSwift
 import RxCocoa
 
 class MakePostSceneViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
+    
+    let tableView = UITableView()
+    let submitButton = UIBarButtonItem()
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -24,11 +29,29 @@ class MakePostSceneViewController: UIViewController {
     }
     
     private func attribute() {
+        title = "글 쓰기"
+        view.backgroundColor = .white
         
+        submitButton.title = "제출"
+        submitButton.style = .done
+        
+        navigationItem.setRightBarButton(submitButton, animated: true)
+        
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .singleLine
+        tableView.tableFooterView = UIView()
+        
+        tableView.register(TitleTextFieldCell.self, forCellReuseIdentifier: "TitleTextFieldCell") // 0
+        tableView.register(DetailWriteFormCell.self, forCellReuseIdentifier: "DetailWriteFormCell")
+        // 0
     }
     
     private func layout() {
+        view.addSubview(tableView)
         
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
     
     func bind() {
