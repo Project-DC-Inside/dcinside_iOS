1d0a2c71e3da14f91519bca4e38c2a23f6a60347
wolframhwang <hzw94@naver.com>
2022-03-13 21:58:18 +0900

    [feat] NoticeBoard 추가 패치
    - 갤러리에서 선택하면 해당 갤러리의 게시판을 보여주는 화면입니다.

---
+++ MainScene/CustomSideMenu/GalleryListScene/NoticeBoard/NoticeBoardViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NoticeBoardViewController.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/03/13.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class NoticeBoardViewController: UIViewController {
+    
+}
---
+++ MainScene/CustomSideMenu/GalleryListScene/NoticeBoard/NoticeBoardViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  NoticeBoardViewModel.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/03/13.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct NoticeBoardViewModel{
+    
+}
