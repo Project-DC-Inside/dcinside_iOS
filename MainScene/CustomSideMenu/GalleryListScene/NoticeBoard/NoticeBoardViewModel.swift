6b143c2fc33362903fd28967dd1e1c52f4cb6f2d
wolframhwang <hzw94@naver.com>
2022-03-14 21:48:59 +0900

    fix: NoticeBoard ViewModel에서 테스트 코드 제거

--- MainScene/CustomSideMenu/GalleryListScene/NoticeBoard/NoticeBoardViewModel.swift
+++ MainScene/CustomSideMenu/GalleryListScene/NoticeBoard/NoticeBoardViewModel.swift
@@ -19,18 +19,11 @@ struct NoticeBoardViewModel{
     let makePostScene: Driver<String>
     
     init(_ galleryName: String?) {
-        print("HH")
         self.galleryName = galleryName
-        
-        addPost.subscribe(onNext: {
-            print("FF")
-        })
         
         makePostScene = addPost
             .map { _ -> String in
-                print("PPP")
                 guard let ret = galleryName else { return "" }
-                print(ret)
                 return ret
             }
             .asDriver(onErrorDriveWith: .empty())
