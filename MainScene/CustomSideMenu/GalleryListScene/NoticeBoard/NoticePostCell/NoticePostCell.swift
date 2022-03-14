b06072baaa977704abc4ddac96cd51ebd097755c
wolframhwang <hzw94@naver.com>
2022-03-14 21:51:03 +0900

    feat: Notice Post Cell (View) Notice Post View Model (ViewModel) 추가
    레이아웃은 따로 추가하지 않았습니다.

---
+++ MainScene/CustomSideMenu/GalleryListScene/NoticeBoard/NoticePostCell/NoticePostCell.swift
@@ -0,0 +1,33 @@
+//
+//  NoticePostCell.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/03/13.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class NoticePostCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Notice Post Cell Error Occured")
+    }
+    
+    private func layout() {
+        
+    }
+    
+    private func attribute() {
+        
+    }
+    
+    func bind() {
+        
+    }
+}
---
+++ MainScene/CustomSideMenu/GalleryListScene/NoticeBoard/NoticePostCell/NoticePostViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  NoticePostViewModel.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/03/13.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct NoticePostViewModel {
+    
+}
