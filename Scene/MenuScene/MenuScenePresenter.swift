09ee90ba8c32f3fbb59bfbdfa84e629cf5451aeb
wolframhwang <hzw94@naver.com>
2022-04-07 00:11:56 +0900

    [feat]: MenuScene 구성을 위한 클래스 생성, API Service 변경

---
+++ Scene/MenuScene/MenuScenePresenter.swift
@@ -0,0 +1,12 @@
+//
+//  MenuScenePresenter.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/04/07.
+//
+
+import Foundation
+
+class MenuScenePresenter: NSObject {
+    
+}
---
+++ Scene/MenuScene/MenuSceneViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MenuSceneViewController.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/04/07.
+//
+
+import Foundation
+import UIKit
+
+class MenuSceneViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
