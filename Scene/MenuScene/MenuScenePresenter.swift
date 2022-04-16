dfba958aba566323ae66edeb02fbc789a5936faa
wolframhwang <hzw94@naver.com>
2022-04-16 23:11:46 +0900

    [feat] MenuScene  전체적인 화면 구성 MVP

--- Scene/MenuScene/MenuScenePresenter.swift
+++ Scene/MenuScene/MenuScenePresenter.swift
@@ -6,7 +6,49 @@
 //
 
 import Foundation
+import UIKit
+
+protocol MenuSceneProtocol: AnyObject {
+    func setUpNavigationBar()
+    func setUpViews()
+    func signInSetUp(signInID: String)
+}
 
 class MenuScenePresenter: NSObject {
+    private weak var viewController: MenuSceneProtocol?
+    private let tableViewItems: [String] = ["메이저 갤러리", "마이너 갤러리", " 미니 갤러리"]
+    
+    init(viewController: MenuSceneProtocol) {
+        self.viewController = viewController
+    }
+    
+    func viewDidLoad() {
+        viewController?.setUpNavigationBar()
+        viewController?.setUpViews()
+    }
+    
+    func viewWillAppear() {
+        guard let signInID = UserDefaults.standard.string(forKey: "singInID") else { return }
+        
+        viewController?.signInSetUp(signInID: signInID)
+    }
+}
+
+extension MenuScenePresenter: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableViewItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = tableViewItems[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
+
+extension MenuScenePresenter: UITableViewDelegate {
     
 }
--- Scene/MenuScene/MenuSceneViewController.swift
+++ Scene/MenuScene/MenuSceneViewController.swift
@@ -6,10 +6,64 @@
 //
 
 import Foundation
+import SnapKit
 import UIKit
 
 class MenuSceneViewController: UIViewController {
+    private lazy var presenter = MenuScenePresenter(viewController: self)
+    private lazy var tableView: UITableView = {
+        let tv = UITableView(frame: .zero, style: .insetGrouped)
+        tv.dataSource = presenter
+        tv.delegate = presenter
+        return tv
+    }()
+    private lazy var settingButton: UIBarButtonItem = {
+        let button = UIBarButtonItem()
+        button.image = UIImage(systemName: "gear")
+        
+        return button
+    }()
+    
+    private lazy var signInButton: UIBarButtonItem = {
+        let button = UIBarButtonItem()
+        button.image = UIImage(systemName: "applelogo")
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        presenter.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presenter.viewWillAppear()
     }
 }
+
+extension MenuSceneViewController: MenuSceneProtocol {
+    func setUpNavigationBar() {
+        navigationItem.title = "로그인 하실래요?"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItems = [settingButton, signInButton]
+    }
+    
+    func setUpViews() {
+        view.addSubview(tableView)
+        view.backgroundColor = .systemBackground
+                
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func signInSetUp(signInID: String) {
+        // To-Do : 받아온 ID 데이터 토대로 해서 로그인 못하게 화면 구성해야함
+    }
+}
+
+extension MenuSceneViewController {
+    
+}
+
