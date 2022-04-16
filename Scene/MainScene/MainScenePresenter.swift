612113b00fce16cf70ed8c899a090b6e45f3e6c1
wolframhwang <hzw94@naver.com>
2022-04-17 00:42:14 +0900

    [feat] Main Scene 추가 반영

---
+++ Scene/MainScene/MainScenePresenter.swift
@@ -0,0 +1,52 @@
+//
+//  MainScenePresenter.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/04/06.
+//
+
+import Foundation
+import UIKit
+
+protocol MainSceneViewProtocol {
+    func setupNavigationBar()
+    func setUpViews()
+    func presentToMenuSelectSceneViewController()
+    func reloadTableView()
+}
+
+
+class MainScenePresenter: NSObject {
+    private let viewController: MainSceneViewProtocol
+    
+    init(viewController: MainSceneViewProtocol) {
+        self.viewController = viewController
+    }
+    
+    func viewDidLoad() {
+        viewController.setupNavigationBar()
+        viewController.setUpViews()
+    }
+    
+    func didTapLeftButton() {
+        viewController.presentToMenuSelectSceneViewController()
+    }
+    
+    func reloadTableView() {
+        viewController.reloadTableView()
+    }
+}
+
+extension MainScenePresenter: MainSceneModelProtocol {
+    
+}
+
+extension MainScenePresenter: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
