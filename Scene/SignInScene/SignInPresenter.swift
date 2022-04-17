c0ce7df96e2b933f722e70c9d4bdbb4892cf2b5c
wolframhwang <hzw94@naver.com>
2022-04-17 21:48:01 +0900

    [feat] SignInScene MVP 추가

---
+++ Scene/SignInScene/SignInPresenter.swift
@@ -0,0 +1,16 @@
+//
+//  SignInPresenter.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/04/17.
+//
+
+import Foundation
+
+protocol SingInSceneProtocol: AnyObject {
+    
+}
+
+class SignInPresenter: NSObject {
+    
+}
---
+++ Scene/SignInScene/SignInSceneViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignInSceneViewController.swift
+//  dcinside_iOS
+//
+//  Created by 황지웅 on 2022/04/17.
+//
+
+import Foundation
+import UIKit
+
+class SingInSceneViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
