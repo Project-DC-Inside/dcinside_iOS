07aa825213057c6f2d8cac43de2aca15bc44e125
wolframhwang <hzw94@naver.com>
2022-04-20 00:08:56 +0900

    [feat] Gallery Add를 추가할수 있는 Scene 반영. presenter는, GalleryList와의 종속성에 의해 생성자로 생성하지 않음.

--- Scene/GalleryListScene/GalleryAddScene/GalleryAddScenePresenter.swift
+++ Scene/GalleryListScene/GalleryAddScene/GalleryAddScenePresenter.swift
@@ -6,16 +6,63 @@
 //
 
 import Foundation
+import UIKit
 
 protocol GalleryAddSceneProtocol: AnyObject {
-    
+    func setViews()
+    func reloadData()
+    func setAlert(title: String, content: String, success: Bool)
 }
 
 class GalleryAddScenePresenter: NSObject {
     private weak var viewController: GalleryAddSceneProtocol?
+    private var presenter: GalleryNamePresenter? = nil
     
     init(viewController: GalleryAddSceneProtocol?) {
         self.viewController = viewController
     }
+    
+    func viewDidLoad() {
+        viewController?.setViews()
+    }
+    
+    func viewWillAppear() {
+        viewController?.reloadData()
+    }
+    
+    func didTappedSubmitButton(type: String) {
+        guard let text = presenter?.getContents() else { return }
+        print("TEXT",text, type)
+        let gallery = Gallery(type: type.uppercased(), name: text)
+        APIService.shared.submitNewGallery(gallery: gallery) { [weak self] res in
+            switch res {
+            case .success(let galleryInfo):
+                if galleryInfo.success {
+                    self?.viewController?.setAlert(title: "성공!", content: "갤러리가 생성되었습니다!", success: galleryInfo.success)
+                } else {
+                    self?.viewController?.setAlert(title: "실패", content: galleryInfo.error?.message ?? "", success: galleryInfo.success)
+                }
+            case .failure(let error):
+                self?.viewController?.setAlert(title: "실패", content: "Network 에러", success: false)
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension GalleryAddScenePresenter: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GalleryNameCells", for: IndexPath(row: 0, section: 0)) as! GalleryNameCells
+        presenter = GalleryNamePresenter(tableViewCell: cell)
+        cell.basicSetting(presenter: presenter!)
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
     
 }
--- Scene/GalleryListScene/GalleryAddScene/GalleryAddSceneViewController.swift
+++ Scene/GalleryListScene/GalleryAddScene/GalleryAddSceneViewController.swift
@@ -6,17 +6,82 @@
 //
 
 import Foundation
+import SnapKit
 import UIKit
 
 class GalleryAddSceneViewController: UIViewController {
     private lazy var presenter = GalleryAddScenePresenter(viewController: self)
+    private let type: String
     
+    init(type: String) {
+        self.type = type
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Error")
+    }
+    
+    private lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = .systemBackground
+        tv.register(GalleryNameCells.self, forCellReuseIdentifier: "GalleryNameCells")
+        tv.dataSource = presenter
+        
+        return tv
+    }()
+    
+    private lazy var submitButton: UIBarButtonItem = {
+        let btn = UIBarButtonItem()
+        btn.title = "제출"
+        btn.target = self
+        btn.action = #selector(didTappedSubmitButton)
+        
+        return btn
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        presenter.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        presenter.viewWillAppear()
+    }
+    
+    @objc func didTappedSubmitButton() {
+        presenter.didTappedSubmitButton(type: type)
     }
 }
 
 extension GalleryAddSceneViewController: GalleryAddSceneProtocol {
+    func setViews() {
+        navigationItem.rightBarButtonItem = submitButton
+        
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
     
+    func reloadData() {
+        tableView.reloadData()
+    }
+    
+    func setAlert(title: String, content: String, success: Bool) {
+        let alertVC = UIAlertController(title: title, message: content, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "확인", style: .default) { [weak self] _ in
+            if success {
+                self?.navigationController?.popViewController(animated: true)
+            }
+        }
+        
+        alertVC.addAction(action)
+        
+        self.present(alertVC, animated: true)
+    }
 }
